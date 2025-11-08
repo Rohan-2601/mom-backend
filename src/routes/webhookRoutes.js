cff8a90a9ad21c1d13e9b6b0f8b6e236657b157a
Rohan Raj <rjha09307@gmail.com>
2025-11-09 02:11:00 +0530

    Change Gmail transporter to use port 465 and secure

--- src/routes/webhookRoutes.js
+++ src/routes/webhookRoutes.js
@@ -7,8 +7,9 @@ const router = express.Router();
 // ✅ Gmail transporter — works on Vercel
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // TLS
+ port: 465,
+secure: true,
+ // TLS
   auth: {
     user: process.env.EMAIL_USER,       // your Gmail address
     pass: process.env.EMAIL_PASSWORD,   // your Gmail App Password
